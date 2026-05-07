4db373b85f40e94192c13c0bc926916f13723deb
Dharanish S S <139471384+dharanishselvarajsumathi@users.noreply.github.com>
2026-05-07 18:12:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
 # Write directly to the app
